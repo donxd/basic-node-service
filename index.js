cbbf0ad22c3d53c531b44ae721e48f413495fd37
donxd <n0s3.xd@gmail.com>
2018-11-10 23:35:18 -0600

    feat(index): add listening and response for index

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hi');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get( '/', (request, response) => {
+  console.log( 'request on index ' );
+  response.json({
+    message: 'hi'
+  });
+});
+
+app.listen( port, () => {
+  console.log( `listening port ${port}` );
+});
